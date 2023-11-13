fe260dab0936a0f164401964131d09323f3062f8
Caleb <calebcoliver@gmail.com>
2023-11-13 18:36:21 -0500

    App Commit

---
+++ my-app/my-react-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. penis perfect
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+          Learn React
+        </a>
+      </header>
+      <body>
+        <h1>Caleb </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
